589467cbd705658790809c51c42ea7c0254b672e
gazeti-annuur <hsemkiwa52@gmail.com>
2026-07-21 23:10:20 +0300

    Update home page with An-Nuur UI

---
+++ frontend/next.config.ts
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // <--- Ongeza huu mstari
+  // mipangilio mingine kama ipo...
+};
+
+module.exports = nextConfig;
